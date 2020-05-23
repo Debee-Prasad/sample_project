3af780e6ab04b795d5d435819839cea30ace4890
Debee-Prasad <63064795+Debee-Prasad@users.noreply.github.com>
2020-05-23 20:54:28 +0530

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,17 @@
+import speech_recognition as sr
+def main():
+    voice = "Recording_4.wav"
+    r= sr.Recognizer()
+    with sr.AudioFile(voice) as source:
+        r.adjust_for_ambient_noise(source)
+        print("Converting Audio file to Text")
+
+        audio = r.listen(source)
+
+        try:
+            print("Converted Audio is :\n"+r.recognize_google(audio))
+        except Exception as e:
+            print(e)
+
+if __name__=="__main__":
+    main()
